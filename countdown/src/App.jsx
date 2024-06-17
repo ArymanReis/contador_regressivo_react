0037a181ef1f56a28715d3b3aa1b29972868099c
Aryman Reis <arymanreis@gmail.com>
2024-06-17 12:32:35 -0300

    Avançando

--- countdown/src/App.jsx
+++ countdown/src/App.jsx
@@ -1,10 +1,24 @@
-import './App.css'
+import Title from './components/Title';
+import Counter from './components/Counter';
+
+import Bebe from "./assets/bebe.jpg";
+
+
+import './App.css';
 
 function App() {
 
   return (
-    <div>
-      <h1>Countdown</h1>
+    <div className="App" style={{ backgroundImage: `url(${Bebe})` }}>
+      <div className="container">
+        <Title title="Contagem regressiva nascimento do bebê." />
+        <div className="countdown-container">
+          <Counter title="Dias" number={2} />
+          <Counter title="Horas" number={2} />
+          <Counter title="Minutos" number={2} />
+          <Counter title="Segundos" number={2} />
+        </div>
+      </div>
     </div>
   )
 }
---
+++ countdown/src/components/Title.jsx
@@ -0,0 +1,13 @@
+import "./Title.css";
+
+import React from 'react'
+
+const Title = ({title}) => {
+    return (
+    <div>
+        <h1 className='title'>{title}</h1>
+    </div>
+    )
+}
+
+export default Title
